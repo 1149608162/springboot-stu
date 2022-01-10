0a989db966585f3ac331fd3496127e800e920271
yuan <1149508162@qq.com>
2022-01-10 22:57:45 +0800

    1

--- src/main/java/com/yuan/filter/LoginFilter.java
+++ src/main/java/com/yuan/filter/LoginFilter.java
@@ -1,5 +1,6 @@
 package com.yuan.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yuan.domain.User;
 import com.yuan.service.impl.UserServiceImpl;
 import com.yuan.utils.JsonData;
@@ -10,10 +11,13 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
 @WebFilter(urlPatterns = "/api/v1/pri/*", filterName = "loginFilter")
 public class LoginFilter implements Filter {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -33,19 +37,39 @@ public class LoginFilter implements Filter {
             token = request.getParameter("token");
         }
 
-        if (StringUtils.isEmpty("token")) {
+        if (!StringUtils.isEmpty(token)) {
 
-            return;
-
-        } else {
-
-            // 判断token是否合法
             User user = UserServiceImpl.sessionMap.get(token);
             if (user != null) {
                 filterChain.doFilter(servletRequest, servletResponse);
+            } else {
+                JsonData jsonData = JsonData.buildError("登录失败， token无效", -2);
+
+                String jsonStr = objectMapper.writeValueAsString(jsonData);
+
+                renderJson(response, jsonStr);
             }
+
+        } else {
+
+            JsonData jsonData = JsonData.buildError("未登录", -3);
+
+            String jsonStr = objectMapper.writeValueAsString(jsonData);
+
+            renderJson(response, jsonStr);
         }
     }
+
+    private void renderJson(HttpServletResponse response, String json) {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        try (PrintWriter writer = response.getWriter()) {
+            writer.println(json);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Override
     public void destroy() {
